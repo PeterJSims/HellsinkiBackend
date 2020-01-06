e6eaac58b6462f00aee87ab5ada5f60fd9ac5550
Peter Sims <ragtimesims@gmail.com>
2020-01-05 19:45:04 -0500

    3.3

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+const persons = [
+	{
+		name: 'Arto Hellas',
+		number: '040-123456',
+		id: 1
+	},
+	{
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+		id: 2
+	},
+	{
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+		id: 3
+	},
+	{
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+		id: 4
+	},
+	{
+		name: 'Peter Sims',
+		number: '555-5555',
+		id: 5
+	}
+];
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello world!</h1>');
+});
+
+app.get('/persons', (req, res) => {
+	res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+	res.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p></div>`);
+});
+
+app.get('/persons/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((person) => person.id === id);
+	res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
