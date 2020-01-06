d33e825d23c0a80f752fcec8eadcbd1d77bef687
Peter Sims <ragtimesims@gmail.com>
2020-01-05 20:28:26 -0500

    3.8

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+morgan.token('body', (req) => JSON.stringify(req.body));
+
 app.use(bodyParser.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 const generateId = () => {
 	return Math.floor(Math.random() * 100000);
